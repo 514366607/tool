093a9cde4d6eb75bdfa8061fbd375f0d58203ff5
hible <514366607@qq.com>
2019-07-11 19:51:43 +0800

    返回err

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/514366607/tools
+
+go 1.12
--- jsonconf.go
+++ jsonconf.go
@@ -6,15 +6,15 @@ import (
 )
 
 // JSONLoad 读取配置
-func JSONLoad(filename string, v interface{}) {
+func JSONLoad(filename string, v interface{}) error {
 	data, err := io.ReadFile(filename)
 	if err != nil {
-		return
+		return err
 	}
 
 	datajson := []byte(data)
 	err = json.Unmarshal(datajson, v)
 	if err != nil {
-		return
+		return err
 	}
 }
