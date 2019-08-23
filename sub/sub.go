915d3d9f592a586cbde0fa46ddea6628e6c41fbe
hible <514366607@qq.com>
2019-08-23 18:16:21 +0800

    modify subNum function

--- sub/sub.go
+++ sub/sub.go
@@ -4,26 +4,8 @@ import (
 	"math"
 )
 
-// SubUint64 相加|相减
-func SubUint64(a uint64, b int64) uint64 {
-	if b < 0 {
-		// 相减
-		b = b - b*2 // 变成正数
-		if int64(a) <= b {
-			return 0
-		}
-		return a - uint64(b)
-	}
-
-	// 相加,防止溢出
-	if a+uint64(b) < a {
-		return math.MaxUint64
-	}
-	return a + uint64(b)
-}
-
-// SubInt8 相加|相减
-func SubInt8(a int8, b int8) int8 {
+// Int8 相加|相减
+func Int8(a int8, b int8) int8 {
 	res := a + b
 	if a > 0 && b > 0 && res < 0 {
 		res = math.MaxInt8
@@ -33,13 +15,37 @@ func SubInt8(a int8, b int8) int8 {
 	return res
 }
 
-// SubInt 相加|相减
-func SubInt(a int, b int) int {
-	return int(SubInt32(int32(a), int32(b)))
+// Uint8 相加|相减
+func Uint8(a uint8, b int8) uint8 {
+	if b < 0 {
+		// 相减
+		b = b - b*2 // 变成正数
+		if a <= uint8(b) {
+			return 0
+		}
+		return a - uint8(b)
+	}
+
+	// 相加,防止溢出
+	var newNum = a + uint8(b)
+	if newNum < a {
+		return math.MaxUint8
+	}
+	return newNum
 }
 
-// SubInt32 相加|相减
-func SubInt32(a int32, b int32) int32 {
+// Int 相加|相减
+func Int(a int, b int) int {
+	return int(Int32(int32(a), int32(b)))
+}
+
+// Uint 相加|相减
+func Uint(a int, b int) int {
+	return int(Uint32(uint32(a), int32(b)))
+}
+
+// Int32 相加|相减
+func Int32(a int32, b int32) int32 {
 	res := a + b
 	if a > 0 && b > 0 && res < 0 {
 		res = math.MaxInt32
@@ -49,8 +55,27 @@ func SubInt32(a int32, b int32) int32 {
 	return res
 }
 
-// SubInt64 相加|相减
-func SubInt64(a int64, b int64) int64 {
+// Uint32 相加|相减
+func Uint32(a uint32, b int32) uint32 {
+	if b < 0 {
+		// 相减
+		b = b - b*2 // 变成正数
+		if a <= uint32(b) {
+			return 0
+		}
+		return a - uint32(b)
+	}
+
+	// 相加,防止溢出
+	var newNum = a + uint32(b)
+	if newNum < a {
+		return math.MaxUint32
+	}
+	return newNum
+}
+
+// Int64 相加|相减
+func Int64(a int64, b int64) int64 {
 	res := a + b
 	if a > 0 && b > 0 && res < 0 {
 		res = math.MaxInt64
@@ -58,4 +83,23 @@ func SubInt64(a int64, b int64) int64 {
 		res = math.MinInt64
 	}
 	return res
+}
+
+// Uint64 相加|相减
+func Uint64(a uint64, b int64) uint64 {
+	if b < 0 {
+		// 相减
+		b = b - b*2 // 变成正数
+		if a <= uint64(b) {
+			return 0
+		}
+		return a - uint64(b)
+	}
+
+	// 相加,防止溢出
+	var newNum = a + uint64(b)
+	if newNum < a {
+		return math.MaxUint64
+	}
+	return newNum
 }
--- sub/sub_test.go
+++ sub/sub_test.go
@@ -5,184 +5,230 @@ import (
 	"testing"
 )
 
-func Test_SubUint64(t *testing.T) {
-	var a = SubUint64(1, 2)
+func Test_Int8(t *testing.T) {
+	var a = Int8(1, 2)
 	if a != 3 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
 
-	a = SubUint64(1, -2)
-	if a != 0 {
-		t.Error("减少失败", a)
-		return
-	}
-
-	a = SubUint64(3, -2)
+	a = Int8(3, -2)
 	if a != 1 {
-		t.Error("减少失败", a)
+		t.Fatal("减少失败", a)
 		return
 	}
 
-	a = SubUint64(math.MaxInt64+1, math.MaxInt64)
-	if a != math.MaxUint64 {
-		t.Error("添加失败", a)
-		return
-	}
-
-	a = SubUint64(math.MaxInt64+2, math.MaxInt64)
-	if a != math.MaxUint64 {
-		t.Error("添加失败", a)
-		return
-	}
-
-	a = SubUint64(math.MaxInt64, math.MaxInt64)
-	if a == math.MaxUint64 {
-		t.Error("添加失败", a)
-		return
-	}
-
-}
-
-func Test_SubInt32(t *testing.T) {
-	var a = SubInt32(1, 2)
-	if a != 3 {
-		t.Error("添加失败", a)
-		return
-	}
-
-	a = SubInt32(3, -2)
-	if a != 1 {
-		t.Error("减少失败", a)
-		return
-	}
-
-	a = SubInt32(-3, -2)
+	a = Int8(-3, -2)
 	if a != -5 {
-		t.Error("减少失败", a)
+		t.Fatal("减少失败", a)
 		return
 	}
 
-	a = SubInt32(math.MaxInt32, math.MaxInt32)
-	if a != math.MaxInt32 {
-		t.Error("添加失败", a)
-		return
-	}
-
-	a = SubInt32(math.MaxInt32, int32(2))
-	if a != math.MaxInt32 {
-		t.Error("添加失败", a)
-		return
-	}
-
-	a = SubInt32(math.MinInt32, -3)
-	if a != math.MinInt32 {
-		t.Error("添加失败", a)
-		return
-	}
-
-}
-
-func Test_SubInt8(t *testing.T) {
-	var a = SubInt8(1, 2)
-	if a != 3 {
-		t.Error("添加失败", a)
-		return
-	}
-
-	a = SubInt8(3, -2)
-	if a != 1 {
-		t.Error("减少失败", a)
-		return
-	}
-
-	a = SubInt8(-3, -2)
-	if a != -5 {
-		t.Error("减少失败", a)
-		return
-	}
-
-	a = SubInt8(math.MaxInt8, math.MaxInt8)
+	a = Int8(math.MaxInt8, math.MaxInt8)
 	if a != math.MaxInt8 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
 
-	a = SubInt8(math.MaxInt8, 2)
+	a = Int8(math.MaxInt8, 2)
 	if a != math.MaxInt8 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
 
-	a = SubInt8(math.MinInt8, -3)
+	a = Int8(math.MinInt8, -3)
 	if a != math.MinInt8 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
-
 }
 
-func Test_SubInt(t *testing.T) {
-	var a = SubInt(1, 2)
+func Test_Uint8(t *testing.T) {
+	var a = Uint8(1, 2)
 	if a != 3 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
 
-	a = SubInt(3, -2)
+	a = Uint8(1, -2)
+	if a != 0 {
+		t.Fatal("减少失败", a)
+		return
+	}
+
+	a = Uint8(3, -2)
 	if a != 1 {
-		t.Error("减少失败", a)
+		t.Fatal("减少失败", a)
 		return
 	}
 
-	a = SubInt(-3, -2)
+	a = Uint8(math.MaxInt8+1, math.MaxInt8)
+	if a != math.MaxUint8 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Uint8(math.MaxInt8+2, math.MaxInt8)
+	if a != math.MaxUint8 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Uint8(math.MaxInt8, math.MaxInt8)
+	if a == math.MaxUint8 {
+		t.Fatal("添加失败", a)
+		return
+	}
+}
+
+func Test_Int(t *testing.T) {
+	var a = Int(1, 2)
+	if a != 3 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Int(3, -2)
+	if a != 1 {
+		t.Fatal("减少失败", a)
+		return
+	}
+
+	a = Int(-3, -2)
 	if a != -5 {
-		t.Error("减少失败", a)
+		t.Fatal("减少失败", a)
 		return
 	}
 
-	a = SubInt(math.MaxInt32, math.MaxInt32)
+	a = Int(math.MaxInt32, math.MaxInt32)
 	if a != math.MaxInt32 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
 
-	a = SubInt(math.MaxInt32, 2)
+	a = Int(math.MaxInt32, 2)
 	if a != math.MaxInt32 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
 
-	a = SubInt(math.MinInt32, -3)
+	a = Int(math.MinInt32, -3)
 	if a != math.MinInt32 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
-
 }
 
-func Test_SubInt64(t *testing.T) {
-	var a = SubInt64(1, 2)
+func Test_Uint(t *testing.T) {
+	var a = Uint(1, 2)
 	if a != 3 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
 		return
 	}
 
-	a = SubInt64(3, -2)
+	a = Uint(1, -2)
+	if a != 0 {
+		t.Fatal("减少失败", a)
+		return
+	}
+
+	a = Uint(3, -2)
 	if a != 1 {
-		t.Error("减少失败", a)
+		t.Fatal("减少失败", a)
 		return
 	}
 
-	a = SubInt64(math.MaxInt64, math.MaxInt64)
-	if a != math.MaxInt64 {
-		t.Error("添加失败", a)
+	a = Uint(math.MaxInt32+1, math.MaxInt32)
+	if a != math.MaxUint32 {
+		t.Fatal("添加失败", a)
 		return
 	}
 
-	a = SubInt64(math.MaxInt64, int64(2))
+	a = Uint(math.MaxInt32+2, math.MaxInt32)
+	if a != math.MaxUint32 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Uint(math.MaxInt32, math.MaxInt32)
+	if a == math.MaxUint32 {
+		t.Fatal("添加失败", a)
+		return
+	}
+}
+
+func Test_Int64(t *testing.T) {
+	var a = Int64(1, 2)
+	if a != 3 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Int64(3, -2)
+	if a != 1 {
+		t.Fatal("减少失败", a)
+		return
+	}
+
+	a = Int64(-3, -2)
+	if a != -5 {
+		t.Fatal("减少失败", a)
+		return
+	}
+
+	a = Int64(math.MaxInt64, math.MaxInt64)
 	if a != math.MaxInt64 {
-		t.Error("添加失败", a)
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Int64(math.MaxInt64, 2)
+	if a != math.MaxInt64 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Int64(math.MinInt64, -3)
+	if a != math.MinInt64 {
+		t.Fatal("添加失败", a)
+		return
+	}
+}
+
+func Test_Uint64(t *testing.T) {
+	var a = Uint64(1, 2)
+	if a != 3 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Uint64(1, -2)
+	if a != 0 {
+		t.Fatal("减少失败", a)
+		return
+	}
+
+	a = Uint64(3, -2)
+	if a != 1 {
+		t.Fatal("减少失败", a)
+		return
+	}
+
+	a = Uint64(math.MaxInt64+1, math.MaxInt64)
+	if a != math.MaxUint64 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Uint64(math.MaxInt64+2, math.MaxInt64)
+	if a != math.MaxUint64 {
+		t.Fatal("添加失败", a)
+		return
+	}
+
+	a = Uint64(math.MaxInt64, math.MaxInt64)
+	if a == math.MaxUint64 {
+		t.Fatal("添加失败", a)
 		return
 	}
 
